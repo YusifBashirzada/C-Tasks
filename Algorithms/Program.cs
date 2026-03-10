c98a89765d0ab5ae08473d0e48a92d22025eb6a2
unknown <Classtime@PC_6_213_9.aztu.edu.az>
2026-03-10 13:04:29 +0400

    tasks

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace Algorithms
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using System.Globalization;
+
+// 25_____________________
+
+//Console.WriteLine("1 ve 100 araliginda bir eded daxil edin:");
+//int eded = Convert.ToInt32(Console.ReadLine());
+
+//if (1 < eded && eded < 100)
+//{
+//    int netice = (int)Math.Pow(eded, 3);
+//    Console.WriteLine("Ededin kubu: " + netice);
+//}
+//else Console.WriteLine("Daxil etdiyiniz eded 1 ve 100 araliginda deyil");
+
+// 26_______________________ Yarimciq
+
+//Console.WriteLine("Adinizi Daxil Edin:");
+//string ad = Console.ReadLine();
+//Console.WriteLine("Soyadinizi Daxil Edin:");
+//string soyad = Console.ReadLine();
+
+//string netice = (string)ad.Concat(soyad);
+
+//Console.WriteLine(netice);
+
+// 27_______________________
+
+//Console.WriteLine("Bir Soz Daxil Edin:");
+//string soz = Console.ReadLine();
+//int sozuzunlugu = soz.Length;
+
+//Console.WriteLine(soz + " - " + sozuzunlugu + " herf");
+
+// 28_______________________
+
+string tapilmalimeyve = "alma";
+
+Console.WriteLine("Oyunun Qaydalari:");
+Console.WriteLine("Bir Meyve adi var onu tapmalisiniz");
+Console.WriteLine("3 sansiniz var");
+Console.WriteLine("3 sans ile tapsaz qazanirsiz, eks halda meglub olursuz");
+Console.WriteLine("Baslayaq");
+Console.WriteLine("Hansi meyve");
+
+
+
+
+// 29_______________________
--- Algorithms/obj/Debug/net8.0/Algorithms.AssemblyInfo.cs
+++ Algorithms/obj/Debug/net8.0/Algorithms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+991c8449091557c2c9488d55da0250afba3a3dd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
